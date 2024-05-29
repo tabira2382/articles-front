b30880850ac07db48d94dd79fa0dac773ecade43
tabira2382 <tabira2382@gmail.com>
2024-05-29 09:24:22 +0900

    feat:AriticleList sort

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 import { fetchArticles, likeArticle } from '@/lib/api';
 import { Article } from '../../articles';
 
-
 const ArticleList: React.FC = () => {
   const [articles, setArticles] = useState<Article[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
@@ -12,7 +11,8 @@ const ArticleList: React.FC = () => {
     const getArticles = async () => {
       try {
         const articlesData = await fetchArticles();
-        setArticles(articlesData);
+        const sortedArticles = articlesData.sort((a: Article, b: Article) => b.article_like_count - a.article_like_count);
+        setArticles(sortedArticles);
       } catch (error) {
         console.error('Failed to fetch articles', error);
       } finally {
@@ -44,18 +44,21 @@ const ArticleList: React.FC = () => {
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-4">
+    <div className="max-w-6xl mx-auto p-4">
       <h1 className="text-3xl font-bold text-center mb-6">Article List</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="flex flex-col gap-6">
         {articles.map(article => (
-          <div key={article.id} className="bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition-shadow duration-300">
-            <img src={article.image_url} alt={article.title} className="w-full h-48 object-cover mb-4 rounded" />  {/* アイキャッチ画像を表示 */}
-            <h2 className="text-xl font-semibold mb-2">{article.title}</h2>
-            <p className="text-gray-600 mb-2">Tags: {article.tag_list}</p>
-            <p className="text-gray-600 mb-2">Likes: {article.likes_count}</p>
-            <a href={article.url} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
-              Read more
-            </a>
+          <div key={article.id} className="bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition-shadow duration-300 flex flex-row items-center">
+            <img src={article.image_url} alt={article.title} className="w-1/4 h-48 object-cover mb-4 rounded mr-4" />  {/* アイキャッチ画像を表示 */}
+            <div className="w-3/4">
+              <h2 className="text-xl font-semibold mb-2">{article.title}</h2>
+              <p className="text-gray-600 mb-2">Tags: {article.tag_list}</p>
+              <p className="text-gray-600 mb-2">Likes: {article.likes_count}</p>
+              <p className="text-gray-600 mb-2">Article Likes: {article.article_like_count}</p>
+              <a href={article.url} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
+                Read more
+              </a>
+            </div>
           </div>
         ))}
       </div>
